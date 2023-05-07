49f6917a6cee26a127fcc67e2bd7d18870941658
yanakin394 <yana.germatskaya@gmail.com>
2023-05-07 22:42:10 +0300

    script

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const taskList = document.querySelector('.task-list');
 
 // Добавляю обработчик событий для добавления задач в список после нажатия кнопки Добавить
 addTaskButton.addEventListener('click', () => {    
+
     // задаю переменную для получения данных, введенных пользователем
     let task = document.getElementById('task_input').value;
 
     //создаю контейнер, для добавления задач одну за одной
-    const divContainer = document.createElement("div_container");
+    const divContainer = document.createElement("div");
     divContainer.classList.add("list__container");
 
     // добавляю задачу, введенную пользователем, в список
@@ -20,43 +21,20 @@ addTaskButton.addEventListener('click', () => {
         </label>`;
     taskList.appendChild(divContainer);
 
-
     // создаю функцию, чтобы инпут был пустым после нажатия кнопки
-    const cleanInput = () => {
-        const getInput = document.getElementById('task_input');
-        getInput.value = '';
-    }
-    cleanInput();
+    /* const cleanInput = () => { */
+    const getInput = document.getElementById('task_input');
+    getInput.value = '';
+   /*  } */
+    /* cleanInput(); */
 
-    //ЦИКЛ для проверки чекбоксов
-    /* const checkboxes = document.getElementById('checkbox');
-    const checkboxesArr = Array.from(checkboxes); */
-    function enableClearTasks () {
-        const checkboxes = document.getElementById('checkbox');
-        for (let i= 0; i < checkboxes.clientHeight; i++) {
-            checkboxes[i].addEventListener('change', function() {
-                if(checkboxes[i].checked) {
-                    clearTasks.removeAttribute("disabled");
-                }
-            })
-        }
-    }
-    enableClearTasks();
+    //активизация кнопки Очистки задач
+    if(clearTasks.disabled == true) {
+        clearTasks.disabled == false
+    }    
+});
+
+// обработчик событий для кнопки Очищения задач
+clearTasks.addEventListener('click', () => {
+    taskList.innerHTML = ``;
 })
-
-
-/* document.getElementById('checkbox').addEventListener('change', () => {
-    clearTasks.disabled = !checkbox.checked;
-}) */
-
-/* function activeCleanButton () {
-    const checkbox = document.getElementById('checkbox');
-    checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
-            clearTasks.disabled = false;
-        } else {
-            clearTasks.disabled = true;
-        }
-    })
-}
-activeCleanButton (); */
